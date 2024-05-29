5ba844d53d2591507a0c55660acc3a4b8933b7e6
ShirleyRM <srm.shirley@gmail.com>
2024-05-28 23:26:15 -0500

    First commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent {
+  username = 'shinnmar';
+  isLoggedIn = true;
+}
